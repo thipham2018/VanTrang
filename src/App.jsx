fc28390a4da455acc0b0c2335f5b4df8088a9118
thipham2018 <trangphamtmp@gmail.com>
2023-08-10 14:27:22 -0400

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-import './App.css'
-import Navbar0 from './components/Navbar0'
-
-
-function App () {
-=======
 import "./App.css";
 import Navbar0 from "./components/Navbar0";
 import Navbar2 from "./components/Navbar2";
->>>>>>> 80a764ae2161c1b01972f68590c3e69f743a00b5
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-<Navbar0/>  
- </>
-  )
-  }
-
-export default App
-=======
       <Navbar2 />
     </>
   );
 }
 
 export default App;
->>>>>>> 80a764ae2161c1b01972f68590c3e69f743a00b5
